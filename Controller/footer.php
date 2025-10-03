6bf055c4f15be4bf50646c04d8e22bb29a9f5f36
harklight0000 <youremail@gmail.com>
2025-10-04 06:53:23 +0700

    first commit

---
+++ Controller/footer.php
@@ -0,0 +1,31 @@
+<br>
+<div class="bg_tree"></div>
+<div class="foot_bg"></div>
+<div class="left_b_bottom">
+    <div class="right_b_bottom">
+        <div class="footer">
+            <div class="left_bottom"></div>
+            <div class="right_bottom"></div>
+        </div>
+    </div>
+</div>
+<div class="copyright" style="line-height: 7px">
+    <p>Bản quyền web thuộc <a href="http://">Lưu Anh Hoàng</a></p>
+    - <a href="/dieu-khoan-su-dung"><small>Điều Khoản Sử Dụng</small></a>
+    <center><br><select name="forma"
+            onchange="if (!window.__cfRLUnblockHandlers) return false; location = this.options[this.selectedIndex].value;"
+            data-cf-modified-b0e1867ca77e058aaeec4468-="">
+            <option value="http://wap.teamobi.com/game/30"><?php echo $Name_Server ?></option>
+        </select></center>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+    type="b0e1867ca77e058aaeec4468-text/javascript"></script>
+<script src="view/static/js/ThreeCanvas.js" type="b0e1867ca77e058aaeec4468-text/javascript"></script>
+<script src="view/static/js/Snow3d.js" type="b0e1867ca77e058aaeec4468-text/javascript"></script>
+<script src="view/static/js/animation.js@v4" type="b0e1867ca77e058aaeec4468-text/javascript"></script>
+<script src="cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+    data-cf-settings="b0e1867ca77e058aaeec4468-|49" defer></script>
+</body>
+
+</html>
